e0c31959ecc5b71b3458324de3f6dd10d28290d2
Elliot Robinson <elliot.robinson@argiopetech.com>
2013-06-25 21:38:13 -0400

    Changes #defines to consts in gBaraffeMag.hpp

--- base9/gBaraffeMag.hpp
+++ base9/gBaraffeMag.hpp
@@ -1,11 +1,9 @@
-#if defined( GBARAFFEMAG_H )
-
-#else
+#ifndef GBARAFFEMAG_H
 #define GBARAFFEMAG_H
 
-#define N_BAR_AGES        30
-#define N_BAR_MASSES      24
-#define N_BAR_FILTS        6
+const int N_BAR_AGES   = 30;
+const int N_BAR_MASSES = 24;
+const int N_BAR_FILTS  = 6;
 
 void loadBaraffe (char *path);
 void getBaraffeMags (double logAge, double mass);
