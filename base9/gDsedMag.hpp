b8c244755441dbeb4de2356e664d732f2c8caf0d
Elliot Robinson <elliot.robinson@argiopetech.com>
2013-06-25 21:41:19 -0400

    Changes #defines to consts in gDsedMag.hpp

--- base9/gDsedMag.hpp
+++ base9/gDsedMag.hpp
@@ -1,13 +1,10 @@
-#if defined( GDSEDMAG_H )
-
-#else
+#ifndef GDSEDMAG_H
 #define GDSEDMAG_H
 
-//#define FILTS              8
-#define N_DSED_Z           9    /* number of metallicities in Chaboyer-Dotter isochrones */
-#define N_DSED_AGES        52   /* number of ages in Chaboyer-Dotter isochonres */
-#define N_DSED_FILTS       8
-#define MAX_DSED_ENTRIES   370
+const int N_DSED_Z         = 9;    /* number of metallicities in Chaboyer-Dotter isochrones */
+const int N_DSED_AGES      = 52;   /* number of ages in Chaboyer-Dotter isochonres */
+const int N_DSED_FILTS     = 8;
+const int MAX_DSED_ENTRIES = 370;
 
 void loadDsed (char *path, int filterSet);
 double deriveDsedAgbTip (double newFeH, double newY, double newAge);
