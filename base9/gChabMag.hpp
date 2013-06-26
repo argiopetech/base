7ce6a3cd603eb53827a6ffce6b7f16d4111a93f9
Elliot Robinson <elliot.robinson@argiopetech.com>
2013-06-25 21:11:47 -0400

    Changes #defines to consts in gChabMag.hpp

--- base9/gChabMag.hpp
+++ base9/gChabMag.hpp
@@ -1,15 +1,11 @@
-#if defined( GCHABMAG_H )
-
-#else
+#ifndef GCHABMAG_H
 #define GCHABMAG_H
 
-#define N_CHAB_FILTS       8
-#define N_CHAB_Z           4    /* number of metallicities in Chaboyer-Dotter isochrones */
-#define N_CHAB_Y           5    /* number of He abundances in Chaboyer-Dotter isochrones */
-#define N_CHAB_AGES        19   /* number of ages in Chaboyer-Dotter isochonres */
-#define LOW                0
-#define HIGH               1
-#define MAX_CHAB_ENTRIES   280
+const int N_CHAB_FILTS     = 8;
+const int N_CHAB_Z         = 4;    /* number of metallicities in Chaboyer-Dotter isochrones */
+const int N_CHAB_Y         = 5;    /* number of He abundances in Chaboyer-Dotter isochrones */
+const int N_CHAB_AGES      = 19;   /* number of ages in Chaboyer-Dotter isochonres */
+const int MAX_CHAB_ENTRIES = 280;
 
 void loadChaboyer (char *path, int filterSet);
 double deriveChabAgbTip (double newFeH, double newY, double newAge);
