124017df381da45b73ac479e31d1afd41a201c22
Elliot Robinson <elliot.robinson@argiopetech.com>
2013-06-28 02:41:58 -0400

    Adds minor compilation fixes to mpiFunc

--- mpiMcmc/mpiFuncs.hpp
+++ mpiMcmc/mpiFuncs.hpp
@@ -1,3 +1,7 @@
+#include <array>
+
+#include "mpiMcmc.hpp"
+
 void make_cholesky_decomp(struct ifmrMcmcControl &ctrl, double **params);
 double logPostStep(struct chain &mc, std::array<double, N_WD_MASS1> &wdMass1Grid, struct cluster &propClust, double fsLike);
 int acceptClustMarg (double logPostCurr, double logPostProp);
