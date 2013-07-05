8d11df5e1d66f608b16f3cf1f1298401bf36206c
Elliot Robinson <elliot.robinson@argiopetech.com>
2013-07-05 03:00:26 -0400

    Updates wdCooling unittests with hard-coded data
    This increases test length, but makes for better error messages.

--- base9/tests/wdCooling_unittest.hpp
+++ base9/tests/wdCooling_unittest.hpp
@@ -6,22 +6,25 @@
 
 TEST(WdCooling, wdMassToTeffAndRadius)
 {
-    double vars[6][5] = {
-        {8.796, 8.6418, 0.711294, 8.90714, 4.23269},
-        {8.796, 8.62552, 0.716454, 8.90406, 4.22367},
-        {8.796, 8.60251, 0.723936, 8.89966, 4.21289},
-        {8.796, 8.58781, 0.728838, 8.89681, 4.20705},
-        {8.796, 8.42118, 0.79179, 8.86338, 4.17788},
-        {8.796, 8.42424, 0.7905, 8.86406, 4.17788}};
-
-
     loadWDCool("/home/elliot/Projects/stellar_evolution/test/hyades2/models/", ALTHAUS);
 
     double logRadius = 0.0;
 
-    for (int i = 0; i < 6; ++i)
-    {
-        ASSERT_NEAR(vars[i][4], wdMassToTeffAndRadius(vars[i][0], 0.36, vars[i][1], vars[i][2], &logRadius), 0.0001);
-        ASSERT_NEAR(vars[i][3], logRadius, 0.0001);
-    }
+    ASSERT_NEAR(4.23269, wdMassToTeffAndRadius(8.796, 0.36, 8.6418, 0.711294, logRadius), 0.0001);
+    ASSERT_NEAR(8.90714, logRadius, 0.0001);
+
+    ASSERT_NEAR(4.22367, wdMassToTeffAndRadius(8.796, 0.36, 8.62552, 0.716454, logRadius), 0.0001);
+    ASSERT_NEAR(8.90406, logRadius, 0.0001);
+
+    ASSERT_NEAR(4.21289, wdMassToTeffAndRadius(8.796, 0.36, 8.60251, 0.723936, logRadius), 0.0001);
+    ASSERT_NEAR(8.89966, logRadius, 0.0001);
+
+    ASSERT_NEAR(4.20706, wdMassToTeffAndRadius(8.796, 0.36, 8.58781, 0.728838, logRadius), 0.0001);
+    ASSERT_NEAR(8.89681, logRadius, 0.0001);
+
+    ASSERT_NEAR(4.17788, wdMassToTeffAndRadius(8.796, 0.36, 8.42118, 0.79179, logRadius), 0.0001);
+    ASSERT_NEAR(8.86338, logRadius, 0.0001);
+
+    ASSERT_NEAR(4.17788, wdMassToTeffAndRadius(8.796, 0.36, 8.42424, 0.7905, logRadius), 0.0001);
+    ASSERT_NEAR(8.86406, logRadius, 0.0001);
 }
