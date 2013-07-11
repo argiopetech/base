c8bf9cdbb9080f87f5f67e7dbdd740e4a2cc195a
Elliot Robinson <elliot.robinson@argiopetech.com>
2013-07-11 17:04:20 -0400

    Temporarily replaces constructor inheritance (not supported in Clang 3.2)

--- base9/WhiteDwarf.hpp
+++ base9/WhiteDwarf.hpp
@@ -1,6 +1,14 @@
 #include <stdexcept>
+#include <string>
 
 class WDBoundsError : public std::range_error
 {
-    using std::range_error::range_error;
+//    using std::range_error::range_error;
+
+  public:
+    explicit WDBoundsError (const std::string& what_arg)
+        : std::range_error(what_arg) {}
+
+    explicit WDBoundsError (const char* what_arg)
+        : std::range_error(what_arg) {}
 };
