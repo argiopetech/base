a3c99434870159fc4d94fbc2ca4adfaad039669a
Elliot Robinson <elliot.robinson@argiopetech.com>
2014-02-17 04:09:22 -0500

    Prunes long-commented portions of samplers

--- base9/samplers.cpp
+++ base9/samplers.cpp
@@ -1,78 +1,9 @@
-/*** Proposes new star properties ***/
-/*** last update:          ***/
-
 #include <random>
 
 #include "Star.hpp"
 #include "samplers.hpp"
 #include "densities.hpp"
 #include "evolve.hpp"
-
-// void propFieldStar (Star *inputStar)
-// {
-//     double rand1;
-
-//     rand1 = genrand_res53 ();
-
-//     if (rand1 > (*inputStar).clustStarProposalDens)
-//         (*inputStar).isFieldStar = 1;
-//     else
-//         (*inputStar).isFieldStar = 0;
-
-// }
-
-// void propMass (Star *inputStar)
-// {
-//     double rand1;
-
-//     rand1 = 2.0 * (*inputStar).UStepSize * ((2.0 * genrand_res53 ()) - 1.0);    // creates uniform random deviate from -2 sigma to +2 sigma
-//     (*inputStar).U += rand1;
-// }
-
-// void propMassRatio (Star *inputStar)
-// {
-//     double rand1;
-
-//     if ((*inputStar).status[0] == WD || inputStar->status[0] == BD)
-//     {
-//         (*inputStar).massRatio = 0.0;   // WD and BD binaries not allowed
-//     }
-//     else
-//     {
-//         rand1 = 2.0 * (*inputStar).massRatioStepSize * (2.0 * genrand_res53 () - 1.0);
-//         (*inputStar).massRatio += rand1;
-//         while ((*inputStar).massRatio < 0.0 || (*inputStar).massRatio > 1.0)
-//         {
-//             if ((*inputStar).massRatio < 0.0)
-//             {
-//                 (*inputStar).massRatio = -(*inputStar).massRatio;       //reflect proposed ratio if out of bounds
-//             }
-//             if ((*inputStar).massRatio > 1.0)
-//             {
-//                 (*inputStar).massRatio = 2.0 - (*inputStar).massRatio;  //reflect proposed ratio if out of bounds
-//             }
-//         }
-//     }
-// }
-
-// void propClustParam (Cluster *clust, int TYPE)
-// {
-//     if (TYPE == AGE_DURING_WANDER)
-//     {
-//         (*clust).parameter[AGE] = gen_norm ((*clust).parameter[AGE], 10.0 * AGEPROPSTEPSIZE);
-//         return;
-//     }
-//     else if (TYPE == AGE)
-//     {
-//         (*clust).parameter[AGE] = (*clust).parameter[AGE] + (*clust).stepSize[AGE] * (genrand_res53 () - 0.5);
-//         return;
-//     }
-//     else
-//     {
-//         (*clust).parameter[TYPE] = (*clust).parameter[TYPE] + sampleT ((*clust).stepSize[TYPE] * (*clust).stepSize[TYPE]);
-//         return;
-//     }
-// }
 
 double sampleT (std::mt19937 &gen, double var, double nu)
 {
--- base9/samplers.hpp
+++ base9/samplers.hpp
@@ -9,12 +9,6 @@
 const double AGEPROPSTEPSIZE = 0.02;
 const int AGE_WANDER         = 100;
 
-// void propFieldStar (Star *inputStar);
-// void propMass (Star *inputstar);
-// void propMassRatio (Star *inputstar);
-// void propClustParam (Cluster *clust, int TYPE);
-// double gen_norm (double mean, double std_dev);
 double sampleT (std::mt19937&, double var, double nu = DOF);
-// double gamdev (double a);
 
 #endif
