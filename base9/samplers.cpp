60faec6299802429f775c5d9a864d3174eeaaac3
Elliot Robinson <elliot.robinson@argiopetech.com>
2014-02-18 14:59:13 -0500

    More code cleanup in sampleT

--- base9/samplers.cpp
+++ base9/samplers.cpp
@@ -8,12 +8,9 @@ double sampleT (std::mt19937 &gen, double var, double nu)
     // do not call this routine with nu = 2, which wouldn't make much sense anyway
     auto logTDens = [=](double x, double mean)
     {
-        double logp = 0;
-        double s;
+        double s = sqrt (nu / (var * (nu - 2)));
 
-        s = sqrt (nu / (var * (nu - 2)));
-
-        logp = log (s) + gamma(nu) - 3.5 * log (1 + pow (s * (x - mean), 2) / nu);
+        double logp = log (s) + gamma(nu) - 3.5 * log (1 + pow (s * (x - mean), 2) / nu);
 
         return logp;
     };
@@ -21,9 +18,8 @@ double sampleT (std::mt19937 &gen, double var, double nu)
 
     double u = 0.0;
     double y = 0.0;
-    double peak = 0.0;
 
-    peak = exp (logTDens (0, 0, var));
+    double peak = exp (logTDens (0, 0));
 
     do
     {
@@ -34,7 +30,7 @@ double sampleT (std::mt19937 &gen, double var, double nu)
         if (y < 1.e-15)
             y = 1.e-15;         /* (TvH) - trap for u = 0; does 53 bit resolution mean 1/(2^53)? */
         y = log (y);
-    } while (y > logTDens (u, 0, var));
+    } while (y > logTDens (u, 0));
 
     return u;
 }
