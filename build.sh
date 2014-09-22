639fe193be3b956a58cc1b49c0ba5f663bbebad4
Elliot Robinson <elliot.robinson@argiopetech.com>
2014-09-22 02:19:36 -0400

    Makes build scripts SH compatible

--- build.sh
+++ build.sh
@@ -1,30 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
 # Get the number of CPUs on the system
 # This is not portable
-if [[ ! $MULTICPUS ]]; then
+if [ ! "$NCPUS" ]; then
     unamestr=`uname`
-    if [[ "$unamestr" == 'Linux' ]]; then
-        MULTICPUS=`grep -c ^processor /proc/cpuinfo`
-    elif [[ "$unamestr" == 'Darwin' ]]; then
+    if [ "$unamestr" == 'Linux' ]; then
+        NCPUS=`grep -c ^processor /proc/cpuinfo`
+    elif [ "$unamestr" == 'Darwin' ] || [ "$unamestr" == 'FreeBSD' ]; then
         NCPUS=`sysctl -n hw.ncpu`
-        export CC=`which clang`
-        export CXX=`which clang++`
     else
         NCPUS=1
     fi
 fi
 
-BASE=`dirname ${0}`
+OWD=$PWD
+BASE=`dirname "${0}"`
 
-pushd $BASE
+cd $BASE
 
-if [[ -e ".git" ]]; then
+if [ -e ".git" ]; then
     # Setup submodules
     git submodule init
     git submodule update
 else
-    if [[ ! -e "yaml-cpp/CMakeLists.txt" ]]; then
+    if [ ! -e "yaml-cpp/CMakeLists.txt" ]; then
         # Manually clone yaml-cpp, removing the directory first
         rm -r yaml-cpp
         git clone https://github.com/argiopetech/yaml-cpp.git --depth 1
@@ -33,17 +32,16 @@ fi
 
 cp cmake/yaml-cpp-CMakeLists.txt yaml-cpp/CMakeLists.txt
 
-pushd ./BUILD
-if [[ $PREFIX ]]; then
-    cmake -DCMAKE_BUILD_TYPE="RELEASE" -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+cd ./BUILD
+if [ "$PREFIX" ]; then
+    cmake -DCMAKE_BUILD_TYPE="RELEASE" -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 else
     cmake -DCMAKE_BUILD_TYPE="RELEASE" -DCMAKE_INSTALL_PREFIX='/usr/local' ..
 fi
-make -j${MULTICPUS}
+make -j"$NCPUS"
 
-if [[ $? == 0 && ! $NO_INSTALL ]]; then
+if [ "$?" == 0 ] && [ ! "$NO_INSTALL" ]; then
     make install
 fi;
 
-popd
-popd
+cd "$OWD"
