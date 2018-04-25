2b8046c1ae590eb1f6c14f6c64fdcaae31b95bb7
Elliot Robinson <elliot.robinson@argiopetech.com>
2018-04-24 23:29:25 -0400

    Implement a basic query and output system for SQL queries
    If applied, this commit will:
    
     - Implement build framework for sqlTools
     - Add new sqlTools directory to the top level CMakeLists
     - Tweak formatting for command line help
     - Add a SQL runner/pretty printer and implement run listing with it
     - Add .dir-locals.el to .gitignore
     - Remove spurious whitespace from some sqlTools files

---
+++ sqlTools/listRuns/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#include "SqlQueryRunner.hpp"
+
+using std::cerr;
+using std::endl;
+
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        cerr << "Usage: " << argv[0] << " TARGET.base9" << endl;
+        return 1;
+    }
+
+    auto sql =
+        "select \"Multi-Pop\" as Program, runId as 'Run Identifier', time as Time, count(iterId) as Iterations"
+        "  from run"
+        "    join multi_pop"
+        "  on run.id = runId"
+        "  group by runId "
+
+        "union "
+
+        "select \"Single Pop\" as program, runId, time, count(iterId)"
+        "  from run"
+        "    join single_pop"
+        "  on run.id = runId"
+        "  group by runId "
+        "order by runId;";
+
+    SqlQueryRunner query(argv[1], sql);
+
+    query.run();
+    query.prettyPrint();
+
+    return 0;
+}
