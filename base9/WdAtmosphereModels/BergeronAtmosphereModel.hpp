91b348fa9b060798a9111b9015042bfe7124255d
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-09-06 19:11:08 -0400

    Change Bergeron's K to Ks

--- base9/WdAtmosphereModels/BergeronAtmosphereModel.hpp
+++ base9/WdAtmosphereModels/BergeronAtmosphereModel.hpp
@@ -14,7 +14,7 @@ class BergeronAtmosphereModel : public WdAtmosphereModel
   public:
     BergeronAtmosphereModel()
     {
-        availableFilters = {"U", "B", "V", "R", "I", "J", "H", "K", "u", "g", "r", "i", "z"
+        availableFilters = {"U", "B", "V", "R", "I", "J", "H", "Ks", "u", "g", "r", "i", "z"
                            ,"y", "F435W", "F475W", "F555W", "F606W", "F625W", "F658N"
                            , "F775W", "F814W", "F850LP"};
     }
