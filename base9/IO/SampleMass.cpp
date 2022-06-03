3ce8b8c7c65d7fe44500f60c3899c00ce7fa8067
Elliot Robinson <elliot.robinson@argiopetech.com>
2022-06-02 22:17:36 -0400

    Change sampleMass results output to a single file

--- base9/IO/SampleMass.cpp
+++ base9/IO/SampleMass.cpp
@@ -11,41 +11,34 @@ using base::utility::format;
 
 
 SampleMass_FileBackingStore::SampleMass_FileBackingStore(string baseName)
-    : FileBackingStore(baseName + ".massSamples"), membership(baseName + ".membership")
-{
-    if(!membership)
-    {
-        throw std::runtime_error(baseName + ".membership was not available for writing.");
-    }
-}
+    : FileBackingStore(baseName + ".sampleMass.out")
+{ ; }
 
 void SampleMass_FileBackingStore::save(vector<SampleMassRecord> data)
 {
-    if (!data.empty() && data.at(0).iter.val == 1)
+    if (!data.empty() && data.front().iter.val == 1)
     {
         header(data);
     }
 
     for ( auto d : data )
     {
-        fout << base::utility::format << d.primaryMass
-             << base::utility::format << d.massRatio;
-
-        membership << base::utility::format << d.clusterMembership;
+        fout << base::utility::format << d.iter.val
+             << base::utility::format << d.starId
+             << base::utility::format << d.primaryMass
+             << base::utility::format << d.massRatio
+             << base::utility::format << d.clusterMembership
+             << endl;
     }
-
-    fout       << endl;
-    membership << endl;
 }
 
 
-void SampleMass_FileBackingStore::header(vector<SampleMassRecord> data)
+void SampleMass_FileBackingStore::header(vector<SampleMassRecord>)
 {
-    for ( auto d : data )
-    {
-        fout << base::utility::format << d.starId + " mass"
-             << base::utility::format << d.starId + " ratio";
-    }
-
-    fout << endl;
+    fout << base::utility::format << " iteration"
+         << base::utility::format << " starId"
+         << base::utility::format << " mass"
+         << base::utility::format << " massRatio"
+         << base::utility::format << " membership"
+         << endl;
 }
